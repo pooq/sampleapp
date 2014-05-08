2dbf720254e7ee8b2698f9b8b3d73c9d1df573a1
hkj <975807293@qq.com>
2014-05-09 00:02:01 +0800

    hh

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,7 +30,9 @@ end
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
     end
+
 
     describe "with valid information" do
       before do
@@ -39,11 +41,15 @@ end
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-
+	  
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
     end
+
+
+
   end
    
 
